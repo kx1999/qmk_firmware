ee14d63cc0f8c94b60eaee64abe637a044be6694
Kyle Xiao <kx1999@gmail.com>
2019-06-13 11:42:50 -0400

    Fixed spacing in iris/keymaps/kylex/rules.mk

--- keyboards/keebio/iris/keymaps/kylex/rules.mk
+++ keyboards/keebio/iris/keymaps/kylex/rules.mk
@@ -1,17 +1,17 @@
-EXTRAKEY_ENABLE = yes       			# Audio control and System control(+450)
-NKRO_ENABLE = yes           			# N-key Rollover
-AUDIO_ENABLE = no           			# Audio output on port C6
-RGBLIGHT_ENABLE = yes        			# Enable WS2812 RGB underlight
-LEADER_ENABLE = no 								# Enable leader key chording
-UNICODE_ENABLE = no         			# Enables Unicode output with WinCompose
-SPLIT_KEYBOARD = yes							# Split keyboard
-NO_USB_STARTUP_CHECK = yes				# Disables usb suspend check after keyboard startup
-TAP_DANCE_ENABLE = yes						# Enables Tap-Dance functionality
-BACKLIGHT_ENABLE = no 	     			# Enable keyboard backlight functionality
-API_SYSEX_ENABLE = no							# Future API functionality (leave disabled)
-AUTO_SHIFT_ENABLE = yes						# Enables auto shift functionality
+EXTRAKEY_ENABLE = yes             # Audio control and System control(+450)
+NKRO_ENABLE = yes                 # N-key Rollover
+AUDIO_ENABLE = no                 # Audio output on port C6
+RGBLIGHT_ENABLE = yes             # Enable WS2812 RGB underlight
+LEADER_ENABLE = no                # Enable leader key chording
+UNICODE_ENABLE = no               # Enables Unicode output with WinCompose
+SPLIT_KEYBOARD = yes              # Split keyboard
+NO_USB_STARTUP_CHECK = yes        # Disables usb suspend check after keyboard startup
+TAP_DANCE_ENABLE = yes            # Enables Tap-Dance functionality
+BACKLIGHT_ENABLE = no             # Enable keyboard backlight functionality
+API_SYSEX_ENABLE = no             # Future API functionality (leave disabled)
+AUTO_SHIFT_ENABLE = yes           # Enables auto shift functionality
 
-KYLEX_SONGS = no									# Includes copyrighted songs
-TAP_DANCE_MACROS = yes						# Includes custom macros
+KYLEX_SONGS = no                  # Includes copyrighted songs
+TAP_DANCE_MACROS = yes            # Includes custom macros
 
-BOOTLOADER = qmk-dfu							# Defines bootloader
+BOOTLOADER = qmk-dfu              # Defines bootloader
