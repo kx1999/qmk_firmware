46d4c48da8220a655e33d7f72a6f470f4404a95d
Kyle Xiao <kx1999@gmail.com>
2019-06-14 10:31:28 -0400

    Formatting.

--- keyboards/crkbd/keymaps/kylex/keymap.c
+++ keyboards/crkbd/keymaps/kylex/keymap.c
@@ -135,9 +135,9 @@ void iota_gfx_task_keymap(void) {
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
+    #ifdef SSD1306OLED
+      set_keylog(keycode, record);
+    #endif
     // set_timelog();
   }
   return true;
