3a0ec654410cae4796963e5ee5cbf55ce04ae549
Kyle Xiao <kx1999@gmail.com>
2019-07-02 06:24:24 -0400

    Enabled RGB Matrix for crkbd.

--- keyboards/crkbd/keymaps/kylex/rules.mk
+++ keyboards/crkbd/keymaps/kylex/rules.mk
@@ -6,7 +6,7 @@
 EXTRAKEY_ENABLE = yes             # Audio control and System control(+450)
 NKRO_ENABLE = yes                 # N-key Rollover
 AUDIO_ENABLE = no                 # Audio output on port C6
-RGBLIGHT_ENABLE = yes             # Enable WS2812 RGB underlight
+RGBLIGHT_ENABLE = no              # Enables standard RGB underglow
 LEADER_ENABLE = no                # Enable leader key chording
 UNICODE_ENABLE = no               # Enables Unicode output with WinCompose
 NO_USB_STARTUP_CHECK = yes        # Disables usb suspend check after keyboard startup
@@ -14,6 +14,8 @@ TAP_DANCE_ENABLE = yes            # Enables Tap-Dance functionality
 BACKLIGHT_ENABLE = no             # Enable keyboard backlight functionality
 API_SYSEX_ENABLE = no             # Future API functionality (leave disabled)
 AUTO_SHIFT_ENABLE = yes           # Enables auto shift functionality
+
+RGB_MATRIX_ENABLE = WS2812		  # Enables RGB matrix functionality
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no             # Breathing sleep LED during USB suspend
@@ -24,10 +26,10 @@ TAP_DANCE_MACROS = yes            # Includes custom macros
 BOOTLOADER = qmk-dfu
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  users/kylex/lib/layer_state_reader.c \
+SRC +=  users/kylex/lib/glcdfont.c \
         users/kylex/lib/logo_reader.c \
+		users/kylex/lib/layer_state_reader.c \
         users/kylex/lib/host_led_state_reader.c \
-        users/kylex/lib/glcdfont.c \
         # users/kylex/lib/rgb_state_reader.c \
         # users/kylex/lib/keylogger.c \
         # users/kylex/lib/mode_icon_reader.c \
