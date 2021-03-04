3623ccb31c654df96646a8b174e7d7afcd3582f6
Kyle Xiao <kx1999@gmail.com>
2021-03-03 23:03:49 -0500

    4x6 changes

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/kylex/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/kylex/rules.mk
@@ -28,6 +28,6 @@ RGB_MATRIX_DRIVER = WS2812
 KYLEX_SONGS = no                  # Includes copyrighted songs
 TAP_DANCE_MACROS = yes            # Includes custom macros
 GAME_MODE = no					  # Enables _GAME layer
-NUMPAD_LAYER = yes 				  # Enables _NUMP layer
+NUMPAD_LAYER = no 			  	  # Enables _NUMP layer
 
 BOOTLOADER = dfu              	  # Defines bootloader
