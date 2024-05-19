b19dff427b9b5b0b23fe298504146352e964a227
kx1999 <kx1999@gmail.com>
2024-05-19 17:46:20 -0400

    Keymap + keycode additions.

--- users/kylex/custom_keycodes.h
+++ users/kylex/custom_keycodes.h
@@ -19,22 +19,22 @@ enum global_keycodes {
 	KC_CLOD
 };
 
-#define KC_         KC_TRNS
-#define KC_____		KC_NO
-#define KC_BACK     KC_WBAK                                                                                                    // Back
-#define KC_FRWD     KC_WFWD                                                                                                    // Forward
+#define KC_LCA0     LCA(KC_0)                                                                                                   // Left Control + Alt + 0 ==== OBS Save Replay Buffer
+#define KC_LCA9     LCA(KC_9)                                                                                                   // Left Control + Alt + 9 ==== OBS Toggle Recording
+#define KC_BACK     KC_WBAK                                                                                                     // Back
+#define KC_FRWD     KC_WFWD                                                                                                     // Forward
 #define KC_CSFT     SFT_T(KC_CAPS_LOCK)                                                                                         // Hold for Shift, tap for Caps Lock
-#define KC_CESC     CTL_T(KC_ESC)                                                                                              // Hold for Ctrl, tap for Esc
+#define KC_CESC     CTL_T(KC_ESC)                                                                                               // Hold for Ctrl, tap for Esc
 
-#define KC_LOWR     MO(_LOWER)                                                                                                 // Tap toggle _LOWER layer
-#define KC_RASE     MO(_RAISE)                                                                                                 // Tap toggle _RAISE layer
+#define KC_LOWR     MO(_LOWER)                                                                                                  // Tap toggle _LOWER layer
+#define KC_RASE     MO(_RAISE)                                                                                                  // Tap toggle _RAISE layer
 #define KC_NAV		MO(_NAV)
-#define KC_NUMP     MO(_NUMPAD)                                                                                                // Toggle _NUMPAD layer
+#define KC_NUMP     MO(_NUMPAD)                                                                                                 // Toggle _NUMPAD layer
 
-#define KC_FTAB     LCTL(KC_TAB)                                                                                               // Next tab
-#define KC_BTAB     LCTL(LSFT(KC_TAB))                                                                                         // Previous tab
-#define KC_HYP      TD(HY)                                                                                                     // HYP: Tap 1 for Printscreen, Tap 2 for Task Manager, Tap 3 to Ctrl+Alt+Del, Tap 4 to Sleep, Tap 5 to Shut Down, Tap 1 and Hold for Meh modifier, Tap 2 and Hold for Hyper modifier
-#define KC_DSCR     TD(DC)                                                                                                     // DSCR: Tap 1 for Discord mute, Tap 2 for Discord deafen, Tap 3 to Toggle _GAME layer
+#define KC_FTAB     LCTL(KC_TAB)                                                                                                // Next tab
+#define KC_BTAB     LCTL(LSFT(KC_TAB))                                                                                          // Previous tab
+#define KC_HYP      TD(HY)                                                                                                      // HYP: Tap 1 for Printscreen, Tap 2 for Task Manager, Tap 3 to Ctrl+Alt+Del, Tap 4 to Sleep, Tap 5 to Shut Down, Tap 1 and Hold for Meh modifier, Tap 2 and Hold for Hyper modifier
+#define KC_DSCR     TD(DC)                                                                                                      // DSCR: Tap 1 for Discord mute, Tap 2 for Discord deafen, Tap 3 to Toggle _GAME layer
 #define KC_GAME		TG(_GAME)
 #define KC_GTST		TG(_GAMETEST)
 #define KC_RSET		RESET
